9aeca7bdae98e2d3b62123e3fbcc86b2673e2b44
miksor2023 <miksor@yandex.ru>
2024-02-19 16:53:20 +0300

    hw 14.02.24

--- src/main/java/ru/hogwarts/school/entity/Student.java
+++ src/main/java/ru/hogwarts/school/entity/Student.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -11,6 +12,7 @@ public class Student {
     private int age;
     @ManyToOne
     @JoinColumn(name = "faculty_id")
+    @JsonIgnore
     private Faculty faculty;
 
 
